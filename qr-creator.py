7f334e435c5edc31419d4caeff4ce19b72b5b63f
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-03-30 11:00:09 +1030

    device info set

--- qr-creator.py
+++ qr-creator.py
@@ -1,1 +1,11 @@
-print("hello world")
+from datetime import date
+deviceFingerPrint = "location,time,language,device-parameters, "
+student = {
+    "FName": "Phoebe",
+    "LName": "Dynevor",
+    "StudentID": "123456789",
+    "class": "IT Project",
+    "Date": date.today(),
+    "Contact": "0412345678",
+    "DeviceHash": str(hash(deviceFingerPrint))
+}
