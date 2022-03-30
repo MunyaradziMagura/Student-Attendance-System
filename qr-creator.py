dd36b637cfbc885f4541c16958ffad212a3a3f7f
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-03-30 12:50:01 +1030

    Added second student  for testing purposes

--- qr-creator.py
+++ qr-creator.py
@@ -1,5 +1,6 @@
 from datetime import date
 import qrcode
+
 # device fingerprinting algorithm
 deviceFingerPrint = "location,time,language,device-parameters, "
 date = date.today()
@@ -13,6 +14,26 @@ student = {
     "DeviceHash": str(hash(deviceFingerPrint))
 }
 
+student2 = {
+    "FName": "Zack",
+    "LName": "Anderson",
+    "StudentID": "110202346",
+    "class": "IT Project",
+    "Date": date,
+    "Contact": "082625093",
+    "DeviceHash": str(hash(deviceFingerPrint))
+}
+
 image = qrcode.make(student)
+image2 = qrcode.make(student2)
+
 type(image)
+
 image.save("qr_code.png")
+image2.save("qr_code2.png")
+
+#VERY basic validation
+if student.get("DeviceHash") == student2.get("DeviceHash"):
+    print("Invalid")
+else:
+    print("Valid")
