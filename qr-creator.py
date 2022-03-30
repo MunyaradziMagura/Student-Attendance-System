c6248b2a3adc4d929ce7d095c94c4f36ee12dd8d
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-03-30 11:16:15 +1030

    added date varible

--- qr-creator.py
+++ qr-creator.py
@@ -2,12 +2,13 @@ from datetime import date
 import qrcode
 # device fingerprinting algorithm
 deviceFingerPrint = "location,time,language,device-parameters, "
+date = date.today()
 student = {
     "FName": "Phoebe",
     "LName": "Dynevor",
     "StudentID": "123456789",
     "class": "IT Project",
-    "Date": date.today(),
+    "Date": date,
     "Contact": "0412345678",
     "DeviceHash": str(hash(deviceFingerPrint))
 }
