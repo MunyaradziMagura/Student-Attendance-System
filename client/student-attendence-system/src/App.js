1f510160613fbfe719f026def7ac85dd575ba89b
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-07-30 12:02:15 +0930

    Removed redundant page routing.

--- client/student-attendence-system/src/App.js
+++ client/student-attendence-system/src/App.js
@@ -3,27 +3,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './styles/index.css';
 import Login from './pages/Login';
-import StudentDashboard from './pages/StudentDashboard';
-import StudentProfile from './pages/StudentProfile';
-import LecturerDashboard from './pages/LecturerDashboard';
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <nav>
-          <Link to="/login" style={{padding: 20}}>Login</Link>
-          <Link to="/studentdashboard" style={{padding: 20}}>Student Dashboard</Link>
-          <Link to="/studentprofile" style={{padding: 20}}>Student Profile</Link>
-          <Link to="/lecturerdashboard" style={{padding: 20}}>Lecturer Dashboard</Link>
-        </nav>
-        <Routes>
-          <Route path="/studentdashboard" element={<StudentDashboard/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/studentprofile" element={<StudentProfile/>}/>
-          <Route path="/lecturerdashboard" element={<LecturerDashboard/>}/>
-        </Routes>
-      </Router>
+      <Login />
     </div>
 
   );
