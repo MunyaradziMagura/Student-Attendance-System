c770523145d5dea65c348abcf6bb7c8415ebb35a
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-07-30 16:45:46 +0930

    Revised App.js file

--- client/student-attendence-system/src/App.js
+++ client/student-attendence-system/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 //import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, parsePath } from 'react-router-dom'
 import './styles/index.css';
 import Login from './pages/Login';
-
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        {/* <Navigation></Navigation> */}
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/StudentHome" element={<StudentHome/>} />
+        </Routes>
+      </Router>
       <Login />
     </div>
 
