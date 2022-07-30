ddcbaab829b9ba31e083707aad4b6b661c168158
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-07-30 16:51:58 +0930

    Added App.js file

---
+++ client/student-attendence-system/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Link, parsePath } from 'react-router-dom'
+import './styles/index.css';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        {/* <Navigation></Navigation> */}
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/StudentHome" element={<StudentHome/>} />
+        </Routes>
+      </Router>
+      <Login />
+    </div>
+
+  );
+}
+
+export default App;
