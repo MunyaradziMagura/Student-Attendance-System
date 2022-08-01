4d7979e7a1b82a7824b4c06ffca665030af55552
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-08-01 17:16:18 +0930

    QR code generation and reading

--- client/student-attendence-system/src/components/QRCode.js
+++ client/student-attendence-system/src/components/QRCode.js
@@ -1,13 +1,30 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import qrcode from 'qrcode';
+import {useEffect, useState} from 'react';
 
 // import image from '../'
 function QRCode(props) {
+
+  // props should take in a person object 
+  const person = {
+    person: "munya",
+    lastName: "magura",
+    classID: 1234456789,
+  }
+
+  const [src, setSrc] = useState('');
+
+  useEffect(() => {
+    // create QR code 
+    qrcode.toDataURL(JSON.stringify(person)).then(setSrc);
+  }, [])
+
   return (
     <div className="d-flex justify-content-around" style={{paddingTop: "3rem", paddingBottom: "3rem"}}>
       <Card border="info" style={{ width: '100%', backgroundColor: "transparent" }}>
-        <Card.Img animation="wave" variant="top" src="images/QR.png" class="img-thumbnail"/>
+        {/* visualise QRCode image */}
+        <Card.Img animation="wave" variant="top" src={src} class="img-thumbnail"/>
         <Card.Body>
           <Card.Title style={{color: "white", fontWeight: "bold", fontSize: "2rem"}}>It Project QR</Card.Title>
           <Card.Text style={{color: "white"}}>
