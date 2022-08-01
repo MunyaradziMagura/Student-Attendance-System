aa90527b30590cdceb381e195d68df23812a58de
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-08-01 16:55:12 +0930

    added qrcode to readme

--- client/student-attendence-system/src/components/StudentClassCard.js
+++ client/student-attendence-system/src/components/StudentClassCard.js
@@ -6,7 +6,7 @@ import Card from 'react-bootstrap/Card';
 import React from "react";
 const StudentClassCard = ({classesObject, varientList}) => {
     return (
-        
+
         <ListGroup>
 
         <ListGroup.Item className="d-flex justify-content-between align-items-start" id="classHeading">CLASSES <Badge id="classHeadingBall" bg="primary" pill>{Object.keys(classesObject).length}</Badge> </ListGroup.Item>
@@ -15,7 +15,7 @@ const StudentClassCard = ({classesObject, varientList}) => {
             // loop through object 
         Object.keys(classesObject).map((key, index) =>
 
-    //  create card clickable background
+    // create card clickable background
     <ListGroup.Item onClick={() => alert("hello")} action variant={varientList[index + 1]} id="studentClasses">
         {/* create card */}
         <Card text={varientList[index].toLowerCase() === 'light' ? 'dark' : 'white'} bg="primary" border={varientList[index -1]} style={{ width: '100%', top: "-2rem" }}>
