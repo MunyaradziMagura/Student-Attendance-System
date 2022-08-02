5bc06f4112cda1b78173fe8641c0afefa06211c7
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-08-02 18:40:44 +0930

    graph complete
    graph is done and the functionality. when user clicks out of the graph it is closed and when the user clicks either the smaller list component or bigger card component a graph is generated

---
+++ client/student-attendence-system/src/components/AttendenceChart.js
@@ -0,0 +1,132 @@
+import React from "react";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Label
+} from "recharts";
+
+const data = [
+  {
+    date: "2/8/2022",
+    Attendce: 4,
+    maxAttendence: 8
+  },
+  {
+    date: "3/8/2022",
+    Attendce: 5,
+    maxAttendence: 8
+  },
+  {
+    date: "4/8/2022",
+    Attendce: 7,
+    maxAttendence: 8
+  },
+  {
+    date: "5/8/2022",
+    Attendce: 8,
+    maxAttendence: 8
+  },
+  {
+    date: "6/8/2022",
+    Attendce: 2,
+    maxAttendence: 8
+  },
+  {
+    date: "7/8/2022",
+    Attendce: 4,
+    maxAttendence: 8
+  }
+  ,
+  {
+    date: "8/8/2022",
+    Attendce: 3,
+    maxAttendence: 8
+  }
+  ,
+  {
+    date: "9/8/2022",
+    Attendce: 1,
+    maxAttendence: 8
+  }
+  ,
+  {
+    date: "10/8/2022",
+    Attendce: 2,
+    maxAttendence: 8
+  }
+  ,
+  {
+    date: "11/8/2022",
+    Attendce: 2,
+    maxAttendence: 8
+  }
+  ,
+  {
+    date: "12/8/2022",
+    Attendce: 4,
+    maxAttendence: 8
+  }
+  ,
+  {
+    date: "13/8/2022",
+    Attendce: 4,
+    maxAttendence: 8
+  },
+  {
+    date: "14/8/2022",
+    Attendce: 4,
+    maxAttendence: 8
+  },
+  {
+    date: "15/8/2022",
+    Attendce: 4,
+    maxAttendence: 8
+  },
+  {
+    date: "16/8/2022",
+    Attendce: 8,
+    maxAttendence: 8
+  }
+];
+
+const AttendenceChart = (props) => {
+  return (
+    
+    <LineChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: -10,
+        bottom: 5
+      }}
+      className="AttendenceChartGraphStyle"
+    >
+      <CartesianGrid/>
+      <XAxis dataKey="date"/>
+      <YAxis label={{ value: 'Attendence', angle: -90, position: 'Left' }}>
+      </YAxis>
+      <Tooltip />
+      <Legend />
+      <Line
+       fill="#8884d8"
+        type="monotone"
+        dataKey="Attendce"
+        stroke="#8884d8"
+        strokeWidth="4"
+        activeDot={{ r: 9 }}
+        dot={{ r: 4 }}
+      />
+    </LineChart>
+  );
+}
+
+export default AttendenceChart
--- client/student-attendence-system/src/components/StudentAttendenceGraph.js
+++ client/student-attendence-system/src/components/StudentAttendenceGraph.js
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import AttendenceChart from './AttendenceChart';
 const StudentAttendenceGraph = (props) => {
     return (
         <Modal
@@ -9,23 +9,19 @@ const StudentAttendenceGraph = (props) => {
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-        <Modal.Header closeButton>
+        <Modal.Header closeButton >
           <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
+            <b style={{color: "white"}}>Your Attendence</b>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
+
+        {/* visualise chart */}
+        <AttendenceChart/>   
+              
         </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
       </Modal>
+
     )
   }
   
