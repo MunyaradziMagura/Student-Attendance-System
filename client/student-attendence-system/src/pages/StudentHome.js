b056d9cd5ece9d5e5d9aa14dacbcc2e141378f7d
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-08-03 16:29:20 +0930

    clean code

--- client/student-attendence-system/src/pages/StudentHome.js
+++ client/student-attendence-system/src/pages/StudentHome.js
@@ -1,54 +1,53 @@
-import StudentNavigation from "../components/StudentNavigation"
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import QRCode from '../components/QRCode'
-import StudentClasses from '../components/StudentClasses'
+import StudentNavigation from "../components/StudentNavigation";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import QRCode from "../components/QRCode";
+import StudentClasses from "../components/StudentClasses";
 import React from "react";
 function StudentHome(props) {
-  // load different components depending on the page 
+  // load different components depending on the page
   const [page, setPage] = React.useState();
   let pageComponent;
-  
+
   // which page has been selected
-  switch (page){
+  switch (page) {
     case "Home":
-      pageComponent = <QRCode/>;
+      pageComponent = <QRCode />;
       break;
     case "Classes":
-      pageComponent = <StudentClasses width={window.screen.width}/>;
+      pageComponent = <StudentClasses width={window.screen.width} />;
       break;
     default:
-      pageComponent = <QRCode/>;
+      pageComponent = <QRCode />;
   }
-  
-  return (
-    <div style={{paddingTop: "2rem" }}>
-      {/* nav */}
-      <StudentNavigation userName={"Kursie"} setPage={setPage}></StudentNavigation>
-      {/* body */}
-      <Container>
-      <Row>
-        {/* left */}
-        <Col></Col>
-        {/* body */}
-        <Col xs={12}>
-        <Container fluid>
-      <Row>
-        <Col>
-        {/* load the right page */}
-          {pageComponent}
 
-        </Col>
-      </Row>
-    </Container>
-        </Col>
-        {/* right */}
-        <Col></Col>
-      </Row>
+  return (
+    <div style={{ paddingTop: "2rem" }}>
+      {/* nav */}
+      <StudentNavigation
+        userName={"Kursie"}
+        setPage={setPage}
+      ></StudentNavigation>
+      <Container>
+        <Row>
+          <Col></Col>
+          {/* body */}
+          <Col xs={12}>
+            <Container fluid>
+              <Row>
+                <Col>
+                  {/* load the right page */}
+                  {pageComponent}
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col></Col>
+        </Row>
       </Container>
     </div>
-  )
+  );
 }
 
-export default StudentHome
+export default StudentHome;
