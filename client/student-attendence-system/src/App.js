e766950f6275d4880f52148e4185b1f8b3b7f625
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-08-03 18:12:13 +0930

    Update App.js

--- client/student-attendence-system/src/App.js
+++ client/student-attendence-system/src/App.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React from "react";
 //import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Link, parsePath } from 'react-router-dom'
-import './styles/index.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  parsePath,
+} from "react-router-dom";
+import "./styles/index.css";
 
-import Login from './pages/Login';
-import StudentHome from './pages/StudentHome'
-import Dashboard from './pages/Dashboard'
+import Login from "./pages/Login";
+import StudentHome from "./pages/StudentHome";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Route>
-        <nav style={{margin: 10}}>
-          <Link to="studentdashboard" style={{padding: 5}}>
+      <Router>
+        <nav style={{ margin: 10 }}>
+          <Link to="studentdashboard" style={{ padding: 5 }}>
             Student Dashboard
           </Link>
-              <Routes>
-                <Route path="studentdashboard" element={<Dashboard/>} />
-              </Routes>
+          <Routes>
+            <Route path="studentdashboard" element={<Dashboard />} />
+          </Routes>
         </nav>
-       </Router>
-
-      
+      </Router>
     </div>
-
   );
 }
 
