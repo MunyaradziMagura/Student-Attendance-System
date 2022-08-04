7c005b2e9d9491ffc146a1297ff47253875431b8
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-08-04 18:51:31 +0930

    made page navigation simpler

--- client/student-attendence-system/src/App.js
+++ client/student-attendence-system/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from "react";
 //import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Link, parsePath } from 'react-router-dom'
-import './styles/index.css';
-import Login from './pages/Login';
-import StudentHome from './pages/StudentHome'
-import Dashboard from './pages/Dashboard'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  parsePath,
+} from "react-router-dom";
+import "./styles/index.css";
+import Login from "./pages/Login";
+import StudentHome from "./pages/StudentHome";
+import Dashboard from "./pages/Dashboard";
+import Navigation from "./components/Navigation";
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Navigation></Navigation> */}
-        
+        <Navigation />
         <Routes>
           <Route path="/Login" element={<Login />} />
           <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/StudentHome" element={<StudentHome/>} />
+          <Route path="/StudentHome" element={<StudentHome />} />
         </Routes>
       </Router>
-      
     </div>
-
   );
 }
 
