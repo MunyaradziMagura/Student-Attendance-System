91305b9dc1cd80f3e73cdab4f541c451a2958581
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-08-10 17:15:40 +0930

    Added mock attendance data for attendance table. Still needing to add in props and filtering of table.

--- client/student-attendence-system/src/components/StudentAttendanceTable.js
+++ client/student-attendence-system/src/components/StudentAttendanceTable.js
@@ -1,13 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table'
-import Dropdown from 'react-bootstrap/Dropdown'
-import { IoCheckmarkCircleSharp } from "react-icons/io5";
-import axios from "axios"
-
+import { IoCheckmarkCircleSharp, IoHandRight } from "react-icons/io5";
+import Form from 'react-bootstrap/Form'
+import Stack from 'react-bootstrap/Stack'
 
 const StudentAttendanceTable = ({classDate, className, attended}) => {
 
+    const attendanceData = {
+        1: {Date: "17/08/2022", className: "Systems Architecture", attended: false, classType: "Lecture"},
+        2: {Date: "25/08/2022", className: "Systems Architecture", attended: true, classType: "Tutorial"},
+        3: {Date: "13/09/2022", className: "Systems Architecture", attended: false, classType: "Workshop"},
+        4: {Date: "26/09/2022", className: "Data Structures", attended: true, classType: "Practical"},
+        5: {Date: "31/09/2022", className: "Systems Analysis", attended: true, classType: "Practical"},
+        6: {Date: "11/10/2022", className: "IT Project 2", attended: true, classType: "Lecture"},
+    }
+    
+
     return (
+        <div>
+            <Stack direction="horizontal" gap={3}>
+                    <Form.Label style={{paddingRight: 5, paddingTop: 5}}>Course:</Form.Label>
+                    <Form.Select defaultValue={"Select Course"}>
+                        <option>Systems Architecture</option>
+                        <option>IT Project 2</option>
+                        <option>Data Structures</option>
+                        <option>Systems Analysis</option>
+                    </Form.Select>
+
+                    <Form.Label style={{paddingRight: 5, paddingTop: 5}}>Class Type:</Form.Label>
+                    <Form.Select defaultValue={"Select Course"}>
+                        <option>Tutorial</option>
+                        <option>Workshop</option>
+                        <option>Practical</option>
+                        <option>Lecture</option>
+                    </Form.Select>
+            </Stack>
             <Table responsive striped bordered >
                 <thead style={{textAlign: "center"}}>
                     <tr>
@@ -17,24 +44,20 @@ const StudentAttendanceTable = ({classDate, className, attended}) => {
                     </tr>
                 </thead>
                 <tbody style={{textAlign: "center"}}>
-                    <tr>
-                        <td>17/08/2022</td>
-                        <td>Systems Architecture</td>
-                        <td><IoCheckmarkCircleSharp></IoCheckmarkCircleSharp></td>
-                    </tr>
-                    <tr>
-                        <td>24/08/2022</td>
-                        <td>Systems Architecture</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>31/08/2022</td>
-                        <td>Systems Architecture</td>
-                        <td><IoCheckmarkCircleSharp></IoCheckmarkCircleSharp></td>                
-                    </tr>
+                    {
+                    Object.keys(attendanceData).map((key) => (
+                        <tr>
+                            <td>{attendanceData[key].Date}</td>
+                            <td>{attendanceData[key].className}</td>
+                            <td>{attendanceData[key].attended ? <IoCheckmarkCircleSharp/> : ""}</td>
+                        </tr>      
+                    ))    
+                    }
+
                 </tbody>
             </Table>
-    )
-}
+        </div>
+    )}
+
 
 export default StudentAttendanceTable
