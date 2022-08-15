d5dee9512c567a17eaa4f1492edf862dbf7fe2eb
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-08-15 17:38:27 +0930

    fixed git merge issues

--- client/student-attendence-system/src/App.js
+++ client/student-attendence-system/src/App.js
@@ -12,10 +12,7 @@ import Login from "./pages/Login";
 import StudentHome from "./pages/StudentHome";
 import Dashboard from "./pages/Dashboard";
 import Navigation from "./components/Navigation";
-<<<<<<< HEAD:src/App.js
 import YourCourse from "./pages/Dashboard-YourCourse";
-=======
->>>>>>> parent of f3e10d1 (Merge branch 'main' into StudentHome):client/student-attendence-system/src/App.js
 function App() {
   return (
     <div className="App">
@@ -24,10 +21,7 @@ function App() {
         <Routes>
           <Route path="/Login" element={<Login />} />
           <Route path="/Dashboard" element={<Dashboard />} />
-<<<<<<< HEAD:src/App.js
           <Route path="/Dashboard/YourCourse" element={<YourCourse />} />
-=======
->>>>>>> parent of f3e10d1 (Merge branch 'main' into StudentHome):client/student-attendence-system/src/App.js
           <Route path="/StudentHome" element={<StudentHome />} />
         </Routes>
       </Router>
--- client/student-attendence-system/src/components/Dashboard.jsx
+++ client/student-attendence-system/src/components/Dashboard.jsx
@@ -1,217 +1,175 @@
-import React, { useState, useEffect } from 'react';
-<<<<<<< HEAD:src/components/Dashboard.jsx
-import sty from '../styles/Dashboard.module.css'
+import React, { useState, useEffect } from "react";
+import sty from "../styles/Dashboard.module.css";
 
 export default function Dashboard() {
-    //Using Hook for the direct to page 
-=======
-import sty from './dashboard.module.css'
+  useEffect(() => {
+    var dateList = document.getElementById("dateList");
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+    var h4 = document.getElementsByTagName("h4")[0];
+    var iNow = 0;
+    prev.onclick = function () {
+      iNow--;
+      calender(iNow);
+    };
+    next.onclick = function () {
+      iNow++;
+      calender(iNow);
+    };
+    calender(iNow);
+    function calender(n) {
+      var date = new Date();
+      var nowDate = date.getDate();
+      date.setMonth(date.getMonth() + n, 1);
+      var year = date.getFullYear();
+      var month = date.getMonth() + 1;
 
-export default function Dashboard() {
-
->>>>>>> parent of f3e10d1 (Merge branch 'main' into StudentHome):client/student-attendence-system/src/components/Dashboard.jsx
-    useEffect(() => {
-        var dateList = document.getElementById("dateList")
-        var prev = document.getElementById("prev")
-        var next = document.getElementById("next")
-        var h4 = document.getElementsByTagName("h4")[0]
-        var iNow = 0
-        prev.onclick = function () {
-            iNow--
-            calender(iNow)
+      h4.innerHTML = year + "/" + month;
+      var week = date.getDay();
+      var nowMonth = date.getMonth();
+      date.setMonth(nowMonth + 1, 0);
+      var allDays = date.getDate();
+      var str = "";
+      for (var i = 0; i < week; i++) {
+        str += "<li></li>";
+      }
+      for (var j = 1; j <= allDays; j++) {
+        if (n > 0) {
+          if ((week + j) % 7 === 0 || (week + j) % 7 === 1) {
+            str += `<li class='${sty.sun}'>${j}</li>`;
+          } else {
+            str += "<li>" + j + "</li>";
+          }
+        } else if (n < 0) {
+          str += `<li class='${sty.ccc}'>` + j + "</li>";
+        } else {
+          if (j < nowDate) {
+            str += `<li class='${sty.ccc}'>` + j + "</li>";
+          } else if (j === nowDate) {
+            str += `<li class='${sty.red}'>` + j + "</li>";
+          } else if ((week + j) % 7 === 0 || (week + j) % 7 === 1) {
+            str += `<li class='${sty.ccc}'>` + j + "</li>";
+          } else {
+            str += "<li>" + j + "</li>";
+          }
         }
-        next.onclick = function () {
-            iNow++
-            calender(iNow)
-        }
-        calender(iNow)
-        function calender(n) {
-            var date = new Date
-            var nowDate = date.getDate()
-            date.setMonth(date.getMonth() + n, 1)
-            var year = date.getFullYear()
-            var month = date.getMonth() + 1
+      }
+      dateList.innerHTML = str;
+    }
+  }, []);
 
-            h4.innerHTML = year + "/" + month
-            var week = date.getDay()
-            var nowMonth = date.getMonth()
-            date.setMonth(nowMonth + 1, 0)
-            var allDays = date.getDate()
-            var str = ""
-            for (var i = 0; i < week; i++) {
-                str += "<li></li>"
-            }
-            for (var j = 1; j <= allDays; j++) {
-                if (n > 0) {
-                    if ((week + j) % 7 === 0 || (week + j) % 7 === 1) {
-                        str += `<li class='${sty.sun}'>${j}</li>`
-                    } else {
-                        str += "<li>" + j + "</li>"
-                    }
-                } else if (n < 0) {
-                    str += `<li class='${sty.ccc}'>` + j + "</li>"
-                } else {
-                    if (j < nowDate) {
-                        str += `<li class='${sty.ccc}'>` + j + "</li>"
-                    } else if (j === nowDate) {
-                        str += `<li class='${sty.red}'>` + j + "</li>"
-                    } else if ((week + j) % 7 === 0 || (week + j) % 7 === 1) {
-                        str += `<li class='${sty.ccc}'>` + j + "</li>"
-                    } else {
-                        str += "<li>" + j + "</li>"
-                    }
-                }
-            }
-            dateList.innerHTML = str
-        }
-    }, [])
-
-
-    return (
-        <div className={sty.box}>
-
-            <div className={sty.left}>
-                <h2>
-                    Welcome Tom!
-                </h2>
-                <div className={sty.emailBox}>
-
-                </div>
-                <div className={sty.navBox}>
-                    <div className={sty.navItem}>
-                        Your Courses
-                    </div>
-                    <div className={sty.navItem}>
-                        Student Attendance
-                    </div>
-                    <div className={sty.navItem}>
-                        Student Search
-                    </div>
-                </div>
-                <div className={sty.calendar} id='calendar'>
-                    <div className={sty.calendarTop}>
-                        <div id='prev' className={sty.prev}>
-                            &lt;
-                        </div>
-                        <h4>2019年10月</h4>
-                        <div id='next' className={sty.next}>
-                            &gt;
-                        </div>
-                    </div>
-                    <ul className={sty.week}>
-                        <li>Su</li>
-                        <li>Mo</li>
-                        <li>Tu</li>
-                        <li>We</li>
-                        <li>Th</li>
-                        <li>Fr</li>
-                        <li>Sa</li>
-                    </ul>
-                    <ul className={sty.dateList} id="dateList"></ul>
-                </div>
-                <div className={sty.btn}>
-                    Log Out
-                </div>
-            </div>
-
-            <div className={sty.right}>
-                <div className={sty.rightTop}>
-                    <h1>Staff Dashboard</h1>
-                    <div>
-                        Dashboard
-                    </div>
-                </div>
-
-                <div className={sty.rightDesc}>
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit.
-                </div>
-
-
-                <div className={sty.rightTable}>
-                    <h3>Todays Classes</h3>
-
-                    <table border="0">
-                        <thead>
-                            <tr>
-                                <th>Class Name</th>
-                                <th>Time</th>
-                                <th>Date</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Systems Desgin Lecture</td>
-                                <td>12:00pm - 2:00pm</td>
-                                <td>17/05/22</td>
-                                <td>
-                                    <div className={sty.actionBtn}>
-                                        Allow Access
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Systems Desgin Lecture</td>
-                                <td>12:00pm - 2:00pm</td>
-                                <td>17/05/22</td>
-                                <td>
-                                    <div className={sty.actionBtn}>
-                                        Allow Access
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Systems Desgin Lecture</td>
-                                <td>12:00pm - 2:00pm</td>
-                                <td>17/05/22</td>
-                                <td>
-                                    <div className={sty.actionBtn}>
-                                        Allow Access
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Systems Desgin Lecture</td>
-                                <td>12:00pm - 2:00pm</td>
-                                <td>17/05/22</td>
-                                <td>
-                                    <div className={sty.actionBtn}>
-                                        Allow Access
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Systems Desgin Lecture</td>
-                                <td>12:00pm - 2:00pm</td>
-                                <td>17/05/22</td>
-                                <td>
-                                    <div className={sty.actionBtn}>
-                                        Allow Access
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                </div>
-
-
-            </div>
-
-
-
+  return (
+    <div className={sty.box}>
+      <div className={sty.left}>
+        <h2>Welcome Tom!</h2>
+        <div className={sty.emailBox}></div>
+        <div className={sty.navBox}>
+          <div className={sty.navItem}>Your Courses</div>
+          <div className={sty.navItem}>Student Attendance</div>
+          <div className={sty.navItem}>Student Search</div>
         </div>
-    );
+        <div className={sty.calendar} id="calendar">
+          <div className={sty.calendarTop}>
+            <div id="prev" className={sty.prev}>
+              &lt;
+            </div>
+            <h4>2019年10月</h4>
+            <div id="next" className={sty.next}>
+              &gt;
+            </div>
+          </div>
+          <ul className={sty.week}>
+            <li>Su</li>
+            <li>Mo</li>
+            <li>Tu</li>
+            <li>We</li>
+            <li>Th</li>
+            <li>Fr</li>
+            <li>Sa</li>
+          </ul>
+          <ul className={sty.dateList} id="dateList"></ul>
+        </div>
+        <div className={sty.btn}>Log Out</div>
+      </div>
 
+      <div className={sty.right}>
+        <div className={sty.rightTop}>
+          <h1>Staff Dashboard</h1>
+          <div>Dashboard</div>
+        </div>
+
+        <div className={sty.rightDesc}>
+          Lorem ipsum dolor sit amet,consectetur adipiscing elit. Lorem ipsum
+          dolor sit amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet,consectetur adipiscing elit.
+        </div>
+
+        <div className={sty.rightTable}>
+          <h3>Todays Classes</h3>
+
+          <table border="0">
+            <thead>
+              <tr>
+                <th>Class Name</th>
+                <th>Time</th>
+                <th>Date</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Systems Desgin Lecture</td>
+                <td>12:00pm - 2:00pm</td>
+                <td>17/05/22</td>
+                <td>
+                  <div className={sty.actionBtn}>Allow Access</div>
+                </td>
+              </tr>
+              <tr>
+                <td>Systems Desgin Lecture</td>
+                <td>12:00pm - 2:00pm</td>
+                <td>17/05/22</td>
+                <td>
+                  <div className={sty.actionBtn}>Allow Access</div>
+                </td>
+              </tr>
+              <tr>
+                <td>Systems Desgin Lecture</td>
+                <td>12:00pm - 2:00pm</td>
+                <td>17/05/22</td>
+                <td>
+                  <div className={sty.actionBtn}>Allow Access</div>
+                </td>
+              </tr>
+              <tr>
+                <td>Systems Desgin Lecture</td>
+                <td>12:00pm - 2:00pm</td>
+                <td>17/05/22</td>
+                <td>
+                  <div className={sty.actionBtn}>Allow Access</div>
+                </td>
+              </tr>
+              <tr>
+                <td>Systems Desgin Lecture</td>
+                <td>12:00pm - 2:00pm</td>
+                <td>17/05/22</td>
+                <td>
+                  <div className={sty.actionBtn}>Allow Access</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
