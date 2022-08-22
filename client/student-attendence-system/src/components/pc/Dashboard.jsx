ab71b7ef8f32bfd5fcb628daca571290a5eab286
michaelpham12 <96228096+michaelpham12@users.noreply.github.com>
2022-08-22 23:16:53 +0930

    (S3-257) Completed
    (S3-257) - Implemented the Switch component for Lecture Dashboard -Completed 100%

--- client/student-attendence-system/src/components/pc/Dashboard.jsx
+++ client/student-attendence-system/src/components/pc/Dashboard.jsx
@@ -4,12 +4,34 @@ import "react-calendar/dist/Calendar.css";
 import Calendar from "react-calendar";
 import Button from "react-bootstrap/Button";
 import AttendanceTakingPopUp from "./AttendanceTakingPopUp";
+import YourCourse from "./YourCourse";
+import StudentSearchProfile from "./StudentSearchProfile";
+import YourCourseScreen from "./YourCourseScreen";
+import Home from "./Home";
 export default function Dashboard() {
  
   var exampleData = { firstName: "Zackary", lastName: "Anderson" };
   const [value, onChange] = useState(new Date());
   // take attendance
-  const [takeAttendance, setTakeAttendance] = React.useState(false);
+
+  const [page, setPage] = React.useState();
+  let pageComponent;
+
+  // which page has been selected
+  switch (page) {
+    case "YourCourse":
+      pageComponent = <YourCourse />;
+      break;
+    case "StudentAttendance":
+      pageComponent = <></>
+      break;
+    case "StudentSearch":
+      pageComponent = <></>;
+      break;
+
+    default:
+      pageComponent = <Home />;
+  }
 
   return (
     <div className={sty.box}>
@@ -19,9 +41,9 @@ export default function Dashboard() {
         </h2>
         <div className={sty.emailBox}></div>
         <div className={sty.navBox}>
-          <div className={sty.navItem}>Your Courses</div>
-          <div className={sty.navItem}>Student Attendance</div>
-          <div className={sty.navItem}>Student Search</div>
+          <div className={sty.navItem} href="#YourCourse" onClick={() => setPage("YourCourse")}>Your Courses</div>
+          <div className={sty.navItem} href="#StudentAttendance" onClick={() => setPage("StudentAttendance")}>Student Attendance</div>
+          <div className={sty.navItem} href="#StudentSearch" onClick={() => setPage("StudentSearch")}>Student Search</div>
           <Calendar onChange={onChange} value={value} />
         </div>
         <div className={sty.btn}>Log Out</div>
@@ -33,114 +55,8 @@ export default function Dashboard() {
           <div>Dashboard</div>
         </div>
 
-        <div className={sty.rightDesc}>
-          Lorem ipsum dolor sit amet,consectetur adipiscing elit. Lorem ipsum
-          dolor sit amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Lorem ipsum dolor sit
-          amet,consectetur adipiscing elit. Graph Graph Graph Graph Graph Graph
-          Graph Graph Graph
-        </div>
-
-        <div className={sty.rightTable}>
-          <h3>Todays Classes</h3>
-
-          <table border="0">
-            <thead>
-              <tr>
-                <th>Class Name</th>
-                <th>Time</th>
-                <th>Date</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Systems Desgin Lecture</td>
-                <td>12:00pm - 2:00pm</td>
-                <td>17/05/22</td>
-                <td>
-                  {/* <div className={sty.actionBtn}>Allow Access</div> */}
-                  <Button
-                    variant="primary"
-                    style={{ width: "100%", fontSize: "0.8rem" }}
-                    onClick={() => setTakeAttendance(true)}
-                  >
-                    Launch Attendance Taking
-                  </Button>
-                  {/* show popup */}
-                  <AttendanceTakingPopUp
-                    show={takeAttendance}
-                    style={{ width: "100%", fontSize: "0.8rem" }}
-                    onHide={() => setTakeAttendance(false)}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>Systems Desgin Lecture</td>
-                <td>12:00pm - 2:00pm</td>
-                <td>17/05/22</td>
-                <td>
-                  <Button
-                    variant="primary"
-                    style={{ width: "100%", fontSize: "0.8rem" }}
-                    onClick={() => setTakeAttendance(true)}
-                  >
-                    Launch Attendance Taking
-                  </Button>
-                </td>
-              </tr>
-              <tr>
-                <td>Systems Desgin Lecture</td>
-                <td>12:00pm - 2:00pm</td>
-                <td>17/05/22</td>
-                <td>
-                  <Button
-                    variant="primary"
-                    style={{ width: "100%", fontSize: "0.7rem" }}
-                    onClick={() => setTakeAttendance(true)}
-                  >
-                    Launch Attendance Taking
-                  </Button>
-                </td>
-              </tr>
-              <tr>
-                <td>Systems Desgin Lecture</td>
-                <td>12:00pm - 2:00pm</td>
-                <td>17/05/22</td>
-                <td>
-                  <Button
-                    variant="primary"
-                    style={{ width: "80%", fontSize: "0.8rem" }}
-                    onClick={() => setTakeAttendance(true)}
-                  >
-                    Launch Attendance Taking
-                  </Button>
-                </td>
-              </tr>
-              <tr>
-                <td>Systems Desgin Lecture</td>
-                <td>12:00pm - 2:00pm</td>
-                <td>17/05/22</td>
-                <td>
-                  <Button
-                    variant="primary"
-                    onClick={() => setTakeAttendance(true)}
-                    style={{ width: "80%", fontSize: "0.8rem" }}
-                  >
-                    Launch Attendance Taking
-                  </Button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className={sty.rightDesc}> 
+          {pageComponent}
         </div>
       </div>
     </div>
