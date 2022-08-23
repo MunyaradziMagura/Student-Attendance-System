1c17f3b37f15b2ae63717605155fc70ace8fb9cf
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-08-23 16:58:33 +0930

    S3-255 Added in comments for all backend files.

--- client/student-attendence-system/backend/api/students.routes.js
+++ client/student-attendence-system/backend/api/students.routes.js
@@ -1,15 +1,19 @@
 import express from "express"
 
 const studentRouter = express.Router()
-import Student from "../models/students.model.js"
+import Student from "../models/students.model.js" //Import the 'students' model for validation when adding new student to MongoDB
 
+//Default endpoint GET request that will return all student information contained in the MongoDB
 studentRouter.route('/').get((req, res) => {
-    Student.find()
+    Student.find() //MongoDB function that will find ALL documents
     .then(student => res.json(student))
     .catch(err => res.status(400).json('Error: ' + err))
 })
-    
+
+//Adds a new student to the ''students' collection in MongoDB using the below fields
 studentRouter.route('/add').post((req, res) => {
+
+    //Various fields that will store information related to student type documents
     const studentID = req.body.studentID;
     const userName = req.body.userName;
     const firstName = req.body.firstName;
@@ -17,6 +21,7 @@ studentRouter.route('/add').post((req, res) => {
     const attendance = req.body.attendance;
     const program = req.body.program;
 
+    //Creates a new student document that uses the imported 'Student' model
     const new_student = new Student({
         studentID,
         userName,
@@ -26,20 +31,21 @@ studentRouter.route('/add').post((req, res) => {
         program
     });
 
-
+    //Saves the newly created student document into the 'students' collection
     new_student.save()
     .then(() => res.json('New User Added!'))
     .catch((err => res.status(400).json('Error: ' + err)))
 });
 
+//Endpoint that performs a GET request to return attendance data for a student
 studentRouter.route('/getAttendance').get(async(request, result) => {
 
+    //Performs a lookup in the MongoDB that will return one document or nothing if not found
     const attendance = await Student.findOne({studentID: request.body.studentID})
     .then(student => result.json(student))
     .catch(err=> result.status(400).json('Error: ' + err))
-
-    console.log(attendance)
 })
 
+//Exports the studentRouter to be used in the 'server.js' file
 export default studentRouter
 
--- client/student-attendence-system/backend/models/course.model.js
+++ client/student-attendence-system/backend/models/course.model.js
@@ -2,7 +2,16 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+//Course schema file for validation of new course type documents
 const courseSchema = new Schema ({
+
+    //For each field of the schema, various properties are provided:
+    //type: determines the data type (e.g. string)
+    //required: if field is required or not
+    //unique: if field must be unique (similar to primary key)
+    //trim: removes leading and trailing whitespace
+    //timestamps: timestamps for document creation and updating
+
     courseName: {
         type: String,
         required: true,
@@ -31,6 +40,8 @@ const courseSchema = new Schema ({
     timestamps: true,   
 });
 
+//Creates a new model and 'course' collection in the MongoDB that conforms to this schema
 const Course = mongoose.model('Course', courseSchema)
 
+//Exports the schema for usage in the 'courses.routes.js' file
 export default Course;
