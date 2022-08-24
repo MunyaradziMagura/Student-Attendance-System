bdf85226adeea2e6bbd6a16637350076d0be5dcb
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-08-24 17:05:15 +0930

    Rebuilt previous iteration of StudentAttendanceTable but without hardcoded data

--- client/student-attendence-system/src/components/pc/StudentAttendanceTable.js
+++ client/student-attendence-system/src/components/pc/StudentAttendanceTable.js
@@ -1,23 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import { IoCheckmarkCircleSharp,  IoCloseCircle, IoSearch} from "react-icons/io5";
-import ListGroup from 'react-bootstrap/ListGroup'
 //import axios from 'axios'
-import Button from 'react-bootstrap/esm/Button';
-import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
+import Table from 'react-bootstrap/esm/Table';
+import InputGroup from 'react-bootstrap/InputGroup'
+import Stack from 'react-bootstrap/Stack'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const StudentAttendanceTable = ({studentID}) => {
 
-    const [selection, setSelection] = useState()
-
     return (
+
         <div>
-            <ListGroup horizontal padding={3}>
-                <ListGroupItem variant='primary'>Date</ListGroupItem>
-                <ListGroupItem variant='primary'>Class Name</ListGroupItem>
-                <ListGroupItem variant='primary'>Attended</ListGroupItem>
-                <Button><IoSearch/></Button>
-            </ListGroup>
+            <Stack direction="horizontal" gap={3}>
+                    {/*Form inputs for the course type dropdown box*/}
+                    <Form.Label style={{paddingRight: 5, paddingTop: 5, fontWeight: "bold"}}>Course:</Form.Label>
+                    <Form.Select key={"courses"} defaultValue={"default"}>
+                        <option value="default" disabled>---Choose Course---</option>
+
+                        
+                    </Form.Select>
+
+                    {/*Form inputs for the class type dropdown box*/}
+                    <Form.Label style={{paddingRight: 5, paddingTop: 5, fontWeight: "bold"}}>Class Type:</Form.Label>
+                    <Form.Select defaultValue={"default"}>
+                        <option value="default" disabled>---Choose Class Type---</option>
+                        
+                    </Form.Select>
+                    <Button><IoSearch/></Button>    
+            </Stack>
+                <Table responsive striped bordered>
+                    <thead style={{textAlign: "center"}}>
+                        <tr>
+                            <th>Date</th>
+                            <th>Class Name</th>
+                            <th>Attended</th>
+                        </tr>
+                    </thead>
+                    <tbody style={{textAlign: "center"}}>
+                        <tr>No data</tr>
+                        <tr>No data</tr>
+
+                    </tbody>
+                </Table>
         </div>
-    )}
+)}
 
 export default StudentAttendanceTable
