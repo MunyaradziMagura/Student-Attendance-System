68cc983025798cbd25897aa7ba66191cced99eeb
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-09-05 13:02:29 +0930

    Reformated attendance popup to use 'Table' type component over 'ListGroup'

--- client/student-attendence-system/src/components/pc/AttendanceTakingPopUp.js
+++ client/student-attendence-system/src/components/pc/AttendanceTakingPopUp.js
@@ -2,6 +2,7 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import ReactTypingEffect from "react-typing-effect";
 import ListGroup from "react-bootstrap/ListGroup";
+import Table from "react-bootstrap/Table"
 
 import React from "react";
 
@@ -28,13 +29,45 @@ function AttendanceTakingPopUp(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
+        <Table responsive striped bordered hover>
+          <thead style={{textAlign: "center"}}>
+            <th>Date/Time</th>
+            <th>Student Full Name</th>
+            <th>Device Hash</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td>Forest Black</td>
+              <td>2124312h313</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Duff Black</td>
+              <td>21h3213b12j</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Peter Black</td>
+              <td>1hj4234j3j</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Jane Black</td>
+              <td>2124312h313</td>
+            </tr>
+          </tbody>
+        </Table>
         {/* MAKE THIS INTO ITS OWN COMPONENT SO IT CAN BE RE-USED */}
         {/* list of students who attend */}
-        <ListGroup defaultActiveKey="#">
+        {/* <ListGroup defaultActiveKey="#"> */}
           {/* individual student */}
-          <ListGroup.Item action onClick={viewStudent}>
+          {/* <ListGroup.Item action onClick={viewStudent}> */}
             {/* Student information */}
-            <ListGroup horizontal>
+
+            
+
+            {/* <ListGroup horizontal>
               <ListGroup.Item as="li">Date/Time</ListGroup.Item>
               <ListGroup.Item as="li">Student Full Name</ListGroup.Item>
               <ListGroup.Item as="li">Device Hash</ListGroup.Item>
@@ -68,7 +101,7 @@ function AttendanceTakingPopUp(props) {
               <ListGroup.Item as="li">2124312h313</ListGroup.Item>
             </ListGroup>
           </ListGroup.Item>
-        </ListGroup>
+        </ListGroup> */}
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>CLOSE</Button>
