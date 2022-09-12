33ba2a7fe3baa74f515cade362356fee69fd493e
Zackary Anderson <42291976+zackary-anderson@users.noreply.github.com>
2022-09-12 11:49:34 +0930

    Removed duplicate 'home' dashboard navigation option

--- client/student-attendence-system/src/components/pc/Dashboard.jsx
+++ client/student-attendence-system/src/components/pc/Dashboard.jsx
@@ -63,14 +63,6 @@ export default function Dashboard() {
             > Dashboard
             </Nav.Link>
             <Nav.Link 
-            style={{color: "#3b4149"}}
-            className={sty.navItem}
-            href="/Dashboard"
-            onClick={() => setPage("Dashboard")}
-            >
-            Home
-            </Nav.Link>
-            <Nav.Link 
               style={{color: "#3b4149"}}
               className={sty.navItem}
               href="#YourCourse"
