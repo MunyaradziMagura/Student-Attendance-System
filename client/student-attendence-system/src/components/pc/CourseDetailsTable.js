6ff06f9a88d850583e3a858798d321cefcfa1fab
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-10-08 13:48:00 +1030

    now visualising data students to lecturer dashboard

--- client/student-attendence-system/src/components/pc/CourseDetailsTable.js
+++ client/student-attendence-system/src/components/pc/CourseDetailsTable.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table'
-
+import Badge from "react-bootstrap/Badge";
 
 function CourseDetailsTable({attendanceString}, props) {
   // split string on ||, then replace single quotes with double quotes, then remove empty spaces from array, then convert string to json object
-  let attandanceArray = attendanceString.split("||").map((e) => e.replaceAll("'", '"')).filter((e) => {if(e.length > 1) return true}).map((e) => JSON.parse(e));
-  console.log(attandanceArray)
-  // let attandanceObject = 
+  let attandanceObject = attendanceString.split("||").map((e) => e.replaceAll("'", '"')).filter((e) => {if(e.length > 1) return true}).map((e) => JSON.parse(e));
   return (
     <Table striped bordered hover>
       <thead>
@@ -15,13 +13,18 @@ function CourseDetailsTable({attendanceString}, props) {
           <th>Student ID</th>
           <th>Device Hash</th>
         </tr>
+        <Badge id="classHeadingBall" bg="primary" pill>
+          {attandanceObject.length} Students
+        </Badge>
       </thead>
       <tbody>
-        <tr>
-          {/* <td></td>
-          <td></td>
-          <td></td> */}
+      {attandanceObject.map(item => 
+        <tr id={item.deviceFingerPrint}>
+          <td>{item.firstName +" "+ item.lastName}</td>
+          <td>{item.userName}</td>
+          <td>{item.deviceFingerPrint}</td>
         </tr>
+         )}
       </tbody>
     </Table>
     
