8d14b5a852f1de23c28bbcb4d26b9fd88bf8164a
michaelpham12 <96228096+michaelpham12@users.noreply.github.com>
2022-11-01 22:50:59 +1030

    Completed-404-Component
    Have designed a route where it will return the 404 Component if the user access to the URL that is not exist within the Router DOM List.

---
+++ client/student-attendence-system/src/components/pc/InvalidComponent.js
@@ -0,0 +1,18 @@
+import { useLocation, useParams } from "react-router-dom";
+import React from "react";
+import Robot from './BrokenRobot.png';
+export default function InvalidComponent ({}, props) {
+    const location = useLocation();
+    const params = useParams();
+    //const { from } = location.state;
+    console.log(params);
+    const result  = Object.keys(params).map((int) => {return params[int]});
+    console.log(result);
+    const output = "The requested URL /" + result+ " was not found on this server. That's all we know";
+    return(<>
+        <div style={{margin:'auto'}}>
+            <p style={{marginTop:'0', fontWeight:500, fontSize:'2rem', color:'#0052a0', width:'800px', lineHeight:1.5}}>{output}</p>
+            <img src={Robot} style={{ Width:'55%', height:'55%', marginLeft: '10%'}} alt="logo"></img>
+        </div>
+    </>)
+}
--- client/student-attendence-system/src/components/pc/Routes.js
+++ client/student-attendence-system/src/components/pc/Routes.js
@@ -5,13 +5,14 @@ import InnerContent from "./InnerContent";
 import Header from "./Header";
 import YourCourse from "./YourCourse";
 import CourseDetails from "./CourseDetails";
+import InvalidComponent from "./InvalidComponent";
 export default function MainRoutes (props){
     const lecturer = JSON.parse(localStorage.getItem("lecturer"));
     const temp1 = lecturer.courseName;
-    console.log(temp1);
     return(
         <Routes>
         <Route path="/" element={<><InnerContent></InnerContent></>}>
+            <Route path="*" element={<><Header pageName={"404"}/><div className={sty.rightDesc}><InvalidComponent/></div></>}/>
             <Route path="/" element={<Navigate replace to="Courses"/>}/>
             <Route path="Courses" element={<><Header pageName={"Your Course"}/><div className={sty.rightDesc}><YourCourse courseList={lecturer.courses}/></div></>}/>
             <Route path="CoursesDetail" element={<><Header pageName={"Course"}/><div className={sty.rightDesc}><CourseDetails courseName={lecturer.courseName} staffID={lecturer.staffID}></CourseDetails></div></>}></Route>
