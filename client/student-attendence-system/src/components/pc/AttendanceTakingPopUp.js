b467823e5fce572042b3c1e172fb10aae6d59c11
michaelpham12 <96228096+michaelpham12@users.noreply.github.com>
2022-11-02 16:37:38 +1030

    Update AttendanceTakingPopUp.js

--- client/student-attendence-system/src/components/pc/AttendanceTakingPopUp.js
+++ client/student-attendence-system/src/components/pc/AttendanceTakingPopUp.js
@@ -88,6 +88,7 @@ export default function AttendanceTakingPopUp(props) {
     })
     
   // clear attendance
+  setResult("");
   setJsonObjectsArray([]);
   array = []
   jsonObjectsArraySet = new Set()
