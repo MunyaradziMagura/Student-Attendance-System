7a2ed56d92f80a1fe565f570ea52489d8e757b9a
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-11-12 12:56:24 +1030

    added button commenting

--- client/student-attendence-system/src/components/pc/CourseDetails.js
+++ client/student-attendence-system/src/components/pc/CourseDetails.js
@@ -240,6 +240,8 @@ export default function CourseDetails ({backFunction, staffID}, props) {
                           <option value= "" selected>Select Class Type</option>
                           {classTypeList.map(classType =>  (<option value={`${classType}`} >{classType}</option>))}
                       </Form.Select>
+                      
+                      {/* button to launch pdf screen */}
                       <Button variant="outline-success" size="lg" style={{ marginTop:"0.2vh", height: "2.5rem",width: "20%", fontSize: "1rem", marginLeft:'1rem', outlineStyle: "solid", outlineColor: "green", outlineWidth: "thin" }} onClick={()=>{setExportShow(true)}}>Export Attendance (PDF)</Button>
 
 
