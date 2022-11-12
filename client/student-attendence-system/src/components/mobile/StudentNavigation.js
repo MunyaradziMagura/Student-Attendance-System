d0172ee025f65e2a09b40b93d55489faff26307e
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-11-12 19:16:32 +1030

    Update StudentNavigation.js

--- client/student-attendence-system/src/components/mobile/StudentNavigation.js
+++ client/student-attendence-system/src/components/mobile/StudentNavigation.js
@@ -1,13 +1,13 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { IoAlbums } from "react-icons/io5";
 
 import {
   IoHome,
   IoPersonCircleOutline,
   IoBook,
   IoLogOutSharp,
+  IoAlbums
 } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
